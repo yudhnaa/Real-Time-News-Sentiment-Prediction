f76c167ad44fb71bd461d9350d6e4c592d328202
yudhnaa <hoanganhduy75@gmail.com>
2024-07-14 03:07:58 +0700

    first upload

---
+++ Kafka-PySpark/consumer-pyspark.py
@@ -0,0 +1,110 @@
+from pyspark.ml import PipelineModel
+import re
+import pickle
+import nltk
+# from nltk.corpus import stopwords
+# from nltk.tokenize import word_tokenize
+from kafka import KafkaConsumer
+from json import loads
+from pymongo import MongoClient
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+from pyspark.ml import PipelineModel
+from sklearn.base import BaseEstimator, TransformerMixin
+import spacy
+import numpy as np
+from spacy.lang.en.stop_words import STOP_WORDS
+stop_words = STOP_WORDS
+import string
+punctuations = string.punctuation
+
+# Download stopwords
+nltk.download('stopwords', quiet=True)
+nltk.download('punkt', quiet=True)
+
+
+#predictors & clean text
+class predictors(TransformerMixin):
+    def transform(self, X, **transform_params):
+        return [clean_text(text) for text in X]
+
+    def fit(self, X, y, **fit_params):
+        return self
+
+    def get_params(self, deep=True):
+        return {}
+def clean_text(text):
+    return text.strip().lower()
+
+# Creating our tokenizer function
+nlp = spacy.load('en_core_web_sm')
+def spacy_tokenizer(sentence):
+    mytokens = nlp(sentence)
+    lemmatized_tokens = []
+    for word in mytokens:
+        lemma = word.lemma_
+        if lemma != "-PRON-":
+            lemma = lemma.lower().strip()
+        else:
+            lemma = word.lower_
+        lemmatized_tokens.append(lemma)
+    final_tokens = [word for word in lemmatized_tokens if word not in stop_words and word not in punctuations]
+    return final_tokens
+
+# Load the model
+headline_model = pickle.load(open("headline_prediction_model.pkl", 'rb'))
+title_model = pickle.load(open("title_prediction_model.pkl", 'rb'))
+
+# Establish connection to MongoDB
+client = MongoClient('localhost', 27017)
+db = client['bigdata_project']
+collection = db['news']
+
+# Assuming you have a SparkSession already created
+spark = SparkSession.builder \
+    .appName("classify news") \
+    .getOrCreate()
+
+# Kafka Consumer
+consumer = KafkaConsumer(
+    'numtest',
+    bootstrap_servers=['localhost:9092'],
+    auto_offset_reset='earliest',
+    enable_auto_commit=True,
+    group_id='my-group',
+    value_deserializer=lambda x: loads(x.decode('utf-8')))
+
+for message in consumer:
+    print(message)
+    title = message.value['Title']
+    headline = message.value['Headline']
+
+    title = clean_text(title)
+    headline = clean_text(headline)
+
+    # print("-> tweet : ", tweet)
+    # print("-> preprocessed_tweet : ", preprocessed_tweet)
+
+    # Prediction
+    X_test_tit = np.array([title], dtype=object)
+    X_test_head = np.array([headline], dtype=object)
+
+    pred_title = title_model.predict(X_test_tit)
+    pred_headline = headline_model.predict(X_test_head)
+
+    # Print prediction
+    print(f"Title:{title} \n-> Sentiment:{pred_title[0, 0]}")
+    print(f"Headline:{headline} \n-> Sentiment:{pred_headline[0, 0]}")
+
+
+    # Prepare document to insert into MongoDB
+    news_doc = {
+        "title": title,
+        "tit_prediction": pred_title[0, 0],
+        "headline": headline,
+        "head_prediction": pred_headline[0, 0]
+    }
+
+    # Insert document into MongoDB collection
+    collection.insert_one(news_doc)
+    print("/"*50)
---
+++ Kafka-PySpark/producer-validation-tweets.py
@@ -0,0 +1,15 @@
+from time import sleep
+import csv
+from kafka import KafkaProducer
+import json
+
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                         value_serializer=lambda x:
+                         json.dumps(x).encode('utf-8'))
+
+with open('test_file.csv') as file_obj:
+    reader_obj = csv.reader(file_obj)
+    for data in reader_obj:
+        print(data)
+        producer.send('numtest', value={'IDLink': data[0], 'Title': data[1], 'Headline': data[2]})
+        sleep(3)
